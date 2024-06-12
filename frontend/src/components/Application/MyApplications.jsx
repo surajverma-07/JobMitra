801c08b24144abdcb2366afa67e969e490f71088
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2024-06-12 14:25:26 +0530

    frontend created

---
+++ frontend/src/components/Application/MyApplications.jsx
@@ -0,0 +1,201 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../../main";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useNavigate ,Link} from "react-router-dom";
+import ResumeModal from "./ResumeModel";
+const MyApplications = () => {
+  const { user } = useContext(Context);
+  const [applications, setApplications] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [resumeImageUrl, setResumeImageUrl] = useState("");
+
+  const { isAuthorized } = useContext(Context);
+  const navigateTo = useNavigate();
+
+  useEffect(() => {
+    try {
+      if (user && user.role === "Employer") {
+        axios
+          .get("https://jobmitra.onrender.com/api/v1/application/employer/getall", {
+            withCredentials: true,
+          })
+          .then((res) => {
+            setApplications(res.data.data);
+          });
+      } else {
+        axios
+          .get("https://jobmitra.onrender.com/api/v1/application/jobseeker/getall", {
+            withCredentials: true,
+          })
+          .then((res) => {
+            setApplications(res.data.data);
+          });
+      }
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  }, [isAuthorized]);
+
+  if (!isAuthorized) {
+    navigateTo("/");
+  }
+
+  const deleteApplication = (id) => {
+    try {
+      axios
+        .delete(`https://jobmitra.onrender.com/api/v1/application/delete/${id}`, {
+          withCredentials: true,
+        })
+        .then((res) => {
+          toast.success(res.data.message);
+          setApplications((prevApplication) =>
+            prevApplication.filter((application) => application._id !== id)
+          );
+        });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  const openModal = (imageUrl) => {
+    setResumeImageUrl(imageUrl);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <section className="my_applications page">
+      {user && user.role === "Job Seeker" ? (
+        <div className="container">
+          <h1>My Applications</h1>
+          {applications.length <= 0 ? (
+            <>
+              {" "}
+              <h4>No Applications Found</h4>{" "}
+            </>
+          ) : (
+            applications.map((element) => {
+              return (
+                <JobSeekerCard
+                  element={element}
+                  key={element._id}
+                  deleteApplication={deleteApplication}
+                  openModal={openModal}
+                />
+              );
+            })
+          )}
+        </div>
+      ) : (
+        <div className="container">
+          <h1>Applications From Job Seekers</h1>
+          {applications.length <= 0 ? (
+            <>
+              <h4>No Applications Found</h4>
+            </>
+          ) : (
+            applications.map((element) => {
+              return (
+                <EmployerCard
+                  element={element}
+                  key={element._id}
+                  openModal={openModal}
+                />
+              );
+            })
+          )}
+        </div>
+      )}
+      {modalOpen && (
+        <ResumeModal imageUrl={resumeImageUrl} onClose={closeModal} />
+      )}
+    </section>
+  );
+};
+
+export default MyApplications;
+
+const isImage = (url) => {
+  return /\.(jpg|jpeg|png)$/i.test(url);
+};
+
+const JobSeekerCard = ({ element, deleteApplication, openModal }) => {
+  return (
+    <>
+      <div className="job_seeker_card">
+        <div className="detail">
+          <p>
+            <span>Name:</span> {element.name}
+          </p>
+          <p>
+            <span>Email:</span> {element.email}
+          </p>
+          <p>
+            <span>Phone:</span> {element.phone}
+          </p>
+          <p>
+            <span>Address:</span> {element.address}
+          </p>
+          <p>
+            <span>CoverLetter:</span> {element.coverLetter}
+          </p>
+        </div>
+        <div className="resume">
+        {isImage(element.resume.url)?
+          <img
+          src={element.resume.url}
+          alt="resume"
+          onClick={() => openModal(element.resume.url)}
+          />
+          :
+          <div className="button">
+            <button><Link to={element.resume.url} target="_blank">View Resume</Link></button>
+          </div>
+          }
+        </div>
+        <div className="btn_area">
+          <button onClick={() => deleteApplication(element._id)}>
+            Delete Application
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const EmployerCard = ({ element, openModal }) => {
+  return (
+    <>
+      <div className="job_seeker_card">
+        <div className="detail">
+          <p>
+            <span>Name:</span> {element.name}
+          </p>
+          <p>
+            <span>Email:</span> {element.email}
+          </p>
+          <p>
+            <span>Phone:</span> {element.phone}
+          </p>
+          <p>
+            <span>Address:</span> {element.address}
+          </p>
+          <p>
+            <span>CoverLetter:</span> {element.coverLetter}
+          </p>
+        </div>
+        <div className="resume">
+          <img
+            src={element.resume.url}
+            alt="resume"
+            onClick={() => openModal(element.resume.url)}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
